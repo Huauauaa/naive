6e28d28368b6a99efdf6dc916a0fca22a92eacdb
Harvey <harvey0379@163.com>
2021-06-09 11:45:47 +0800

    ✨ count demo

--- src/components/NaiveDashboard.tsx
+++ src/components/NaiveDashboard.tsx
@@ -1,10 +1,41 @@
-import { defineComponent } from 'vue';
-import { NButton } from 'naive-ui';
+import { defineComponent, ref } from 'vue';
+import { NButton, NInput, NSpace } from 'naive-ui';
 import '../assets/naive-dashboard.scss';
 
 export default defineComponent({
   name: 'NaiveDashboard',
   setup() {
-    return () => <NButton>{{ default: () => 'Star Kirby' }}</NButton>;
+    const count = ref(0);
+    const step = ref('1');
+
+    const onCountChange =
+      (step = 1) =>
+      () => {
+        count.value += step;
+      };
+
+    return () => (
+      <fieldset class="naive-dashboard">
+        <legend>Naive Dashboard</legend>
+        <NSpace align="center">
+          <NButton onClick={onCountChange(+step.value)} size="small">
+            +
+          </NButton>
+          <NInput
+            defaultValue={step.value}
+            size="small"
+            type="input"
+            placeholder="Basic Input"
+            onUpdateValue={(value: string) => {
+              step.value = value;
+            }}
+          />
+          {count.value}
+          <NButton onClick={onCountChange(-step.value)} size="small">
+            -
+          </NButton>
+        </NSpace>
+      </fieldset>
+    );
   },
 });
